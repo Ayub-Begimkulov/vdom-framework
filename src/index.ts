93f2d8a0905792571e5e96b1fe42e9d3acb31da9
Ayub Begimkulov <begimkulov963@gmail.com>
2020-01-08 17:04:52 +0300

    made a testing app

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,39 @@
 import createElement from './vdom/create-element';
-import { render } from './vdom/render';
+import render from './vdom/render';
+import mount from './vdom/mount';
+import diff from './vdom/diff';
 
-const app = createElement('div', { attrs: { id: 'hello' } }, [
-  createElement('h1', { attrs: { class: 'heading' } }, ['Hello world']),
-  createElement('input', {
-    attrs: { type: 'text' },
-    events: { input: e => console.log((<HTMLInputElement>e.target).value) },
-    style: { backgroundColor: 'red', border: 'none' }
-  })
-]);
+let word = 'hello';
 
-render(app, '#app');
+const getChildren = (word: string) => {
+  const children = [
+    createElement('h1', { attrs: { class: 'heading' } }, [
+      `Hello world ${word}`
+    ]),
+    createElement('input', {
+      attrs: { type: 'text' },
+      events: { input: e => update((<HTMLInputElement>e.target).value) },
+      style: { border: '1px solid #ccc' }
+    })
+  ];
+
+  if (word) {
+    children.push(createElement('p', {}, [word]));
+  }
+
+  return children;
+};
+
+const app = (word: string) =>
+  createElement('div', { attrs: { id: `hello ${word}` } }, getChildren(word));
+
+let VApp = app(word);
+let root = mount(render(VApp), <HTMLElement>document.querySelector('#app'));
+
+const update = (word: string) => {
+  const newVApp = app(word);
+  const patch = diff(VApp, newVApp);
+  // @ts-ignore
+  root = patch(root);
+  VApp = newVApp;
+};
