2521de27beca5f6410bfd76ee2b5155ee29bb177
Ayub Begimkulov <begimkulov963@gmail.com>
2020-01-10 09:50:30 +0300

    made a createComponent function

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,69 @@
-import createElement from './vdom/create-element';
+import createElement, { VNode } from './vdom/create-element';
 import render from './vdom/render';
 import mount from './vdom/mount';
 import diff from './vdom/diff';
 
-let word = 'hello';
+type CreateComponentFunction = (componentObject: {
+  state: ComponentState;
+  renderFn: (state: ComponentState) => VNode;
+}) => HTMLElement | Text;
 
-const getChildren = (word: string) => {
-  const children = [
-    createElement('h1', { attrs: { class: 'heading' } }, [
-      `Hello world ${word}`
-    ]),
-    createElement('input', {
-      attrs: { type: 'text' },
-      events: { input: e => update((<HTMLInputElement>e.target).value) },
-      style: { border: '1px solid #ccc' }
-    })
-  ];
+interface ComponentState {
+  [key: string]: any;
+}
 
-  if (word) {
-    children.push(createElement('p', {}, [word]));
+const createComponent: CreateComponentFunction = ({ renderFn, state }) => {
+  const componentState = new Proxy(state, {
+    get(obj, prop: string) {
+      return obj[prop];
+    },
+
+    set(obj, prop: string, newVal) {
+      obj[prop] = newVal;
+      const newVCompoent = renderFn(obj);
+      const patch = diff(VComponent, newVCompoent);
+      // @ts-ignore
+      dom = patch(dom);
+      VComponent = newVCompoent;
+
+      return true;
+    }
+  });
+
+  let VComponent = renderFn(componentState);
+  let dom = render(VComponent);
+
+  return dom;
+};
+
+const App = createComponent({
+  state: {
+    word: 'hello'
+  },
+  renderFn: state => {
+    const children = [
+      createElement('h1', { attrs: { class: 'heading' } }, [
+        `Hello world ${state.word}`
+      ]),
+      createElement('input', {
+        attrs: { type: 'text' },
+        events: {
+          input: e => (state.word = (<HTMLInputElement>e.target).value)
+        },
+        style: { border: '1px solid #ccc' }
+      })
+    ];
+
+    if (state.word) {
+      children.push(createElement('p', {}, [state.word]));
+    }
+
+    return createElement(
+      'div',
+      { attrs: { id: `hello ${state.word}` } },
+      children
+    );
   }
+});
 
-  return children;
-};
-
-const app = (word: string) =>
-  createElement('div', { attrs: { id: `hello ${word}` } }, getChildren(word));
-
-let VApp = app(word);
-let root = mount(render(VApp), <HTMLElement>document.querySelector('#app'));
-
-const update = (word: string) => {
-  const newVApp = app(word);
-  const patch = diff(VApp, newVApp);
-  // @ts-ignore
-  root = patch(root);
-  VApp = newVApp;
-};
+mount(App, <HTMLElement>document.getElementById('app'));
